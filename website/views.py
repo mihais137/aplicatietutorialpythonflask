5e6e548283ac75b3af3db76465786bd3228dd940
Alecs21 <ciolacualex32@yahoo.com>
2023-02-21 21:02:11 +0200

    modificari aduse la quiz

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, session, url_for
 import random
+import time
 
 views = Blueprint('views', __name__)
 
@@ -48,18 +49,40 @@ def quiz():
     raspuns_corect1 = raspunsuri1[1]
     raspunsuri1_lista = [item for item in raspunsuri1[0]]
     raspunsuri1_lista.append(raspunsuri1[1])
+    random.shuffle(raspunsuri1_lista) #noua aditie: am randomizat lista de raspunsuri ca sa nu mai fie cel corect ultimul mereu
+
+    intrebare2 = session["intrebare2"]
+    raspunsuri2 = intrebari[intrebare2]
+    raspuns_corect2 = raspunsuri2[1]
+    raspunsuri2_lista = [item for item in raspunsuri2[0]]
+    raspunsuri2_lista.append(raspunsuri2[1])
+    random.shuffle(raspunsuri2_lista)
 
     # trebuie facut pentru n intrebari
     
+    session["punctaj"] = 0
     if request.method == 'POST':
-        selected_option = request.form['select_option']
-        if selected_option == raspuns_corect1:
-            session["punctaj"] = 50 # se va adauga in db punctjul
+        selected_option_1 = request.form.get('select_option1')
+        print (selected_option_1)
+        if selected_option_1 == raspuns_corect1:
+            print("a")
+            session["punctaj"] += 50 # se va adauga in db punctajul
         else:
-            session["punctaj"] = 0
+            session["punctaj"] += 0
+            print("b")
+
+        selected_option_2 = request.form.get('select_option2')
+        print (selected_option_2)
+        if selected_option_2 == raspuns_corect2:
+            print("da")
+            session["punctaj"] += 50
+        else:
+            print("nu")
+            session["punctaj"] += 0
         return redirect(url_for('views.results'))
     else:
-        return render_template('quiz.html', intrebare1 = intrebare1, raspunsuri1_lista = raspunsuri1_lista)
+        return render_template('quiz.html', intrebare1 = intrebare1, raspunsuri1_lista = raspunsuri1_lista, intrebare2 = intrebare2,
+                               raspunsuri2_lista = raspunsuri2_lista)
     
 
 @views.route('/results', methods = ['GET', 'POST'])
@@ -69,7 +92,10 @@ def results():
                 "Intrebarea 3?": [["rasp1", "rasp2", "rasp3"], "rasp_c"]}
     intrebare1 = session["intrebare1"]
     rasp_corect1 = intrebari[intrebare1][1]
+    intrebare2 = session["intrebare2"]
+    rasp_corect2 = intrebari[intrebare2][1]
 
     punctaj = session["punctaj"]
 
-    return render_template('results.html', punctaj = punctaj, rasp_corect1 = rasp_corect1, intrebare1 = intrebare1)
+    return render_template('results.html', punctaj = punctaj, rasp_corect1 = rasp_corect1, intrebare1 = intrebare1,
+                           rasp_corect2 = rasp_corect2, intrebare2 = intrebare2)
